9da1265c6e727a31ab3b1e93b5b570751f779bf4
Rajko Stojadinovic <admin@rajko.info>
2018-08-14 15:08:45 +0200

    Version number bump to 0.7.3

--- ipl/main.c
+++ ipl/main.c
@@ -2676,7 +2676,7 @@ ment_t ment_top[] = {
 };
 menu_t menu_top = {
 	ment_top,
-	"hekate - RajNX edition v0.7.2", 0, 0
+	"hekate - RajNX edition v0.7.3", 0, 0
 };
 
 extern void pivot_stack(u32 stack_top);
